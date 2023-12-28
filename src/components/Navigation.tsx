b6692fac983745e22679c4260e620b84d89a1c52
Yonde <alisan4299@gmail.com>
2023-12-28 18:41:06 +0100

    add projets

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,67 +18,6 @@ const Navigation = () => {
           >
             <li>Contact</li>
           </NavLink>
-          <li className="nav-portfolio">
-            Portfolio
-            <ul className="nav-projects">
-              <NavLink
-                to="/projet-1"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 1</li>
-              </NavLink>
-              <NavLink
-                to="/projet-2"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 2</li>
-              </NavLink>
-              <NavLink
-                to="/projet-3"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 3</li>
-              </NavLink>
-              <NavLink
-                to="/projet-4"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 4</li>
-              </NavLink>
-              <NavLink
-                to="/projet-5"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 5</li>
-              </NavLink>
-              <NavLink
-                to="/projet-6"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 6</li>
-              </NavLink>
-              <NavLink
-                to="/projet-7"
-                className={(nav) =>
-                  nav.isActive ? "nav-active hover" : "hover"
-                }
-              >
-                <li>Projet 7</li>
-              </NavLink>
-            </ul>
-          </li>
         </ul>
       </nav>
     );
